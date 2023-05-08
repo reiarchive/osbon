c3023dc9e35bc6a41d9650ea40e81178180d062b
Kgs. Wahyu <kgswahyu80@gmail.com>
2023-05-08 23:23:05 +0700

    fixing connection database.js

--- src/lib/database.js
+++ src/lib/database.js
@@ -5,6 +5,6 @@ require('dotenv').config({
 const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect('mongodb://127.0.0.1:27017/dbVoting')
 
 module.exports = mongoose
