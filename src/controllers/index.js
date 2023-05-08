7273f1882311626f3c50f7037d66b7501a3afe0d
Kgs. Wahyu <kgswahyu80@gmail.com>
2023-05-08 23:22:37 +0700

    fixing controller index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,6 @@ const Register = require('./RegisterControllers')
 const Voting = require('./VotingControllers')
 const Admin = require('./AdminControllers')
 const Kandidat = require('./KandidatControllers')
+const Guest = require('./GuestController')
 
-module.exports = { Register, Voting, Admin, Kandidat }
+module.exports = { Register, Voting, Admin, Kandidat, Guest }
